faf4cde66e9125ed5e46fa5cd641bcc5fa2ee1aa
Takatsuka-Mark <44513903+Takatsuka-Mark@users.noreply.github.com>
2020-11-17 19:05:25 -0800

    Add Multi-Variable Function Support (#16)
    * Added support for n-variable functions.

--- src/main/java/com/takatsuka/web/math/interpreter/Evaluator.java
+++ src/main/java/com/takatsuka/web/math/interpreter/Evaluator.java
@@ -3,15 +3,17 @@ package com.takatsuka.web.math.interpreter;
 import com.takatsuka.web.interpreter.Function;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Evaluator {
-  public static double evaluateFunction(Function function, List<String> args){
+  public static double evaluateFunction(Function function, List<String> args) {
     switch (function) {
       case UNKNOWN_FUNCTION:
         // The function is unknown, just return the args if available.
         return Double.parseDouble(args.get(0));
 
-      // Symbol Functions (Excluding FAC)
+        // Symbol Functions (Excluding FAC)
       case ADD:
         return Double.parseDouble(args.get(0)) + Double.parseDouble(args.get(1));
       case SUBTRACT:
@@ -23,7 +25,7 @@ public class Evaluator {
       case MOD:
         return Double.parseDouble(args.get(0)) % Double.parseDouble(args.get(1));
 
-      // Mono Variable Functions (Excluding FAC);
+        // Mono Variable Functions (Excluding FAC)
       case ABSOLUTE_VALUE:
         return Math.abs(Double.parseDouble(args.get(0)));
       case SQUARE_ROOT:
@@ -32,6 +34,11 @@ public class Evaluator {
         return Math.sin(Double.parseDouble(args.get(0)));
       case COSINE:
         return Math.cos(Double.parseDouble(args.get(0)));
+
+        // N Variable Functions
+      case MAX:
+        Optional<Double> result = args.stream().map(Double::parseDouble).max(Double::compare);
+        return result.orElse(0.0D); // TODO(mark): This should return some known default value.
     }
     return 0.0D;
   }
--- src/main/java/com/takatsuka/web/math/interpreter/FunctionMapper.java
+++ src/main/java/com/takatsuka/web/math/interpreter/FunctionMapper.java
@@ -11,8 +11,8 @@ import java.util.regex.Pattern;
 
 public class FunctionMapper {
 
-  private static final Map<String, Function> MONO_VARIABLE_FUNCTION_MAP =
-      generateMonoVariableFunctionMap();
+  private static final Map<String, Function> MULTI_VARIALBE_FUNCTION_MAP =
+      generateFunctionMap();
   private static final Map<String, Function> SYMBOL_OPERATOR_MAP = generateSymbolOperatorMap();
   private static final Map<Function, Integer> FUNCTION_TO_MAX_ARG_MAP =
       generateFunctionToMaxArgsMap();
@@ -21,8 +21,8 @@ public class FunctionMapper {
   private static final String numRegex = "(-?\\d+(.\\d+)?)";
 
   public static Function mapStringToFunction(String token) {
-    if (MONO_VARIABLE_FUNCTION_MAP.containsKey(token)) {
-      return MONO_VARIABLE_FUNCTION_MAP.get(token);
+    if (MULTI_VARIALBE_FUNCTION_MAP.containsKey(token)) {
+      return MULTI_VARIALBE_FUNCTION_MAP.get(token);
     }
     if (SYMBOL_OPERATOR_MAP.containsKey(token)) {
       return SYMBOL_OPERATOR_MAP.get(token);
@@ -30,12 +30,16 @@ public class FunctionMapper {
     return null;
   }
 
-  public static int mapFunctionToMaxArgs(Function function) {
+  public static int getMaxArgs(Function function) {
     return FUNCTION_TO_MAX_ARG_MAP.get(function);
   }
 
-  public static boolean isMonoVariableFunction(String symbol) {
-    return MONO_VARIABLE_FUNCTION_MAP.containsKey(symbol);
+  public static boolean isMultiVariableFunction(String symbol) {
+    return MULTI_VARIALBE_FUNCTION_MAP.containsKey(symbol);
+  }
+
+  public static boolean isMultiVariableFunction(Function function) {
+    return MULTI_VARIALBE_FUNCTION_MAP.containsValue(function);
   }
 
   public static boolean isSymbolFunction(String symbol) {
@@ -47,12 +51,13 @@ public class FunctionMapper {
   }
 
   // TODO(mark) Support non-spaced expressions
-  private static Map<String, Function> generateMonoVariableFunctionMap() {
+  private static Map<String, Function> generateFunctionMap() {
     HashMap<String, Function> tmp = new HashMap<>();
     tmp.put("abs", Function.ABSOLUTE_VALUE);
     tmp.put("sin", Function.SINE);
     tmp.put("cos", Function.COSINE);
     tmp.put("sqrt", Function.SQUARE_ROOT);
+    tmp.put("max", Function.MAX);
     return tmp;
   }
 
@@ -63,13 +68,12 @@ public class FunctionMapper {
     tmp.put("*", Function.MULTIPLY);
     tmp.put("/", Function.DIVIDE);
     tmp.put("%", Function.MOD);
-    tmp.put("!", Function.FACTORIAL);
     return tmp;
   }
 
   public static Set<String> getAllSymbols() {
     Set<String> tmp = new HashSet<>();
-    tmp.addAll(MONO_VARIABLE_FUNCTION_MAP.keySet());
+    tmp.addAll(MULTI_VARIALBE_FUNCTION_MAP.keySet());
     tmp.addAll(SYMBOL_OPERATOR_MAP.keySet());
     return tmp;
   }
@@ -78,7 +82,8 @@ public class FunctionMapper {
     ArrayList<String> patterns = new ArrayList<>();
     patterns.add(numRegex);
     patterns.add(SymbolGroups);
-    patterns.addAll(MONO_VARIABLE_FUNCTION_MAP.keySet());
+    patterns.add(",");
+    patterns.addAll(MULTI_VARIALBE_FUNCTION_MAP.keySet());
 
     return Pattern.compile(String.join("|", patterns));
   }
@@ -101,6 +106,7 @@ public class FunctionMapper {
     tmp.put(Function.SINE, 1);
     tmp.put(Function.COSINE, 1);
     tmp.put(Function.SQUARE_ROOT, 1);
+    tmp.put(Function.MAX, Integer.MAX_VALUE);
     return tmp;
   }
 }
