31ebb5532cf62929c4e44cde33efd7b9993ad447
Takatsuka-mark <takatsuka.mark@gmail.com>
2020-11-17 23:23:40 -0800

    Added big-decimal support.

--- src/main/java/com/takatsuka/web/math/interpreter/Evaluator.java
+++ src/main/java/com/takatsuka/web/math/interpreter/Evaluator.java
@@ -2,44 +2,63 @@ package com.takatsuka.web.math.interpreter;
 
 import com.takatsuka.web.interpreter.Function;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Evaluator {
-  public static double evaluateFunction(Function function, List<String> args) {
+
+  private static final String DEFAULT = BigDecimal.ZERO.toString();
+  private final MathContext mathContext;
+
+  public Evaluator() {
+    this(100);
+  }
+
+  public Evaluator(int precision) {
+    mathContext = new MathContext(precision);
+  }
+
+  public String evaluateFunction(Function function, List<String> args) {
     switch (function) {
       case UNKNOWN_FUNCTION:
         // The function is unknown, just return the args if available.
-        return Double.parseDouble(args.get(0));
+        return args.get(0);
 
         // Symbol Functions (Excluding FAC)
       case ADD:
-        return Double.parseDouble(args.get(0)) + Double.parseDouble(args.get(1));
+        return String.valueOf(
+            new BigDecimal(args.get(0)).add(new BigDecimal(args.get(1)), mathContext));
       case SUBTRACT:
-        return Double.parseDouble(args.get(0)) - Double.parseDouble(args.get(1));
+        return String.valueOf(
+            new BigDecimal(args.get(0)).subtract(new BigDecimal(args.get(1)), mathContext));
       case MULTIPLY:
-        return Double.parseDouble(args.get(0)) * Double.parseDouble(args.get(1));
+        return String.valueOf(
+            new BigDecimal(args.get(0)).multiply(new BigDecimal(args.get(1)), mathContext));
       case DIVIDE:
-        return Double.parseDouble(args.get(0)) / Double.parseDouble(args.get(1));
+        return String.valueOf(
+            new BigDecimal(args.get(0)).divide(new BigDecimal(args.get(1)), mathContext));
       case MOD:
-        return Double.parseDouble(args.get(0)) % Double.parseDouble(args.get(1));
+        return String.valueOf(new BigInteger(args.get(0)).mod(new BigInteger(args.get(1))));
 
         // Mono Variable Functions (Excluding FAC)
       case ABSOLUTE_VALUE:
-        return Math.abs(Double.parseDouble(args.get(0)));
+        return String.valueOf(new BigDecimal(args.get(0)).abs(mathContext));
       case SQUARE_ROOT:
-        return Math.sqrt(Double.parseDouble(args.get(0)));
-      case SINE:
-        return Math.sin(Double.parseDouble(args.get(0)));
-      case COSINE:
-        return Math.cos(Double.parseDouble(args.get(0)));
+        return String.valueOf(new BigDecimal(args.get(0)).sqrt(mathContext));
 
         // N Variable Functions
       case MAX:
-        Optional<Double> result = args.stream().map(Double::parseDouble).max(Double::compare);
-        return result.orElse(0.0D); // TODO(mark): This should return some known default value.
+        Optional<BigDecimal> result = args.stream().map(BigDecimal::new).max(BigDecimal::compareTo);
+        if (result.isPresent()) {
+          return result.get().toString();
+        } else {
+          return DEFAULT;
+        }
     }
-    return 0.0D;
+    return DEFAULT;
   }
 }
