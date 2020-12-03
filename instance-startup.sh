bd2013d7a279f9fa6b1a139a37cfc0608912d566
Takatsuka-mark <takatsuka.mark@gmail.com>
2020-12-03 00:08:20 -0800

    Migrated to Gradle!

---
+++ instance-startup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# Following: https://cloud.google.com/community/tutorials/kotlin-springboot-compute-engine
+
+# Set the metadata server to the get project id
+PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+BUCKET=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/BUCKET" -H "Metadata-Flavor: Google")
+
+echo "Project ID: ${PROJECTID} Bucket: ${BUCKET}"
+
+# Get the files we need
+gsutil cp -r gs://"${BUCKET}"/ .
+
+# Install dependencies
+apt-get update
+apt-get -y --force-yes install openjdk-11-jdk
+
+# Make Java 8 default
+update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/jre/bin/java
+
+# Start server
+cd ./website-backend-001
+java -jar web-0.0.1-SNAPSHOT.jar
