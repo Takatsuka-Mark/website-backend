a85336d9d8f906397a5001f0a048817f4b01aef9
Takatsuka-mark <takatsuka.mark@gmail.com>
2021-01-05 17:41:51 -0800

    Updated startup script.

--- instance-startup.sh
+++ instance-startup.sh
@@ -5,11 +5,12 @@
 # Set the metadata server to the get project id
 PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
 BUCKET=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/BUCKET" -H "Metadata-Flavor: Google")
+JARFILE=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/JARFILE" -H "Metadata-Flavor: Google")
 
 echo "Project ID: ${PROJECTID} Bucket: ${BUCKET}"
 
 # Get the files we need
-gsutil cp -r gs://"${BUCKET}"/build001.jar .
+gsutil cp -r gs://"${BUCKET}"/"${JARFILE}" .
 
 # Install dependencies
 apt-get update
@@ -19,4 +20,4 @@ apt-get -y --force-yes install openjdk-11-jdk
 update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/jre/bin/java
 
 # Start server
-java -jar build001.jar
+java -jar /website-backend-builds/"${JARFILE}"
