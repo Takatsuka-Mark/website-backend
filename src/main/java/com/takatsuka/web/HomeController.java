b14b80d81923dd7e6239a53e17354e1566e54333
Takatsuka-Mark <44513903+Takatsuka-Mark@users.noreply.github.com>
2022-05-19 23:36:59 -0400

    Add version endpoint. (#49)

--- src/main/java/com/takatsuka/web/HomeController.java
+++ src/main/java/com/takatsuka/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.takatsuka.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class HomeController {
+    @Autowired
+    BuildProperties buildProperties;
+
     @GetMapping("/isUp")
     public String isUp() {
         return "Yes!";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return buildProperties.getVersion();
     }
 }
